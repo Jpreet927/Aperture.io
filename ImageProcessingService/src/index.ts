4a0206db654f67d096f6aebbd76e4560c0c9fb30
Jaipreet Singh <jaipreet.singh1@ontariotechu.net>
2023-09-19 17:13:19 -0400

    :sparkles: added cloud storage functions

--- ImageProcessingService/src/index.ts
+++ ImageProcessingService/src/index.ts
@@ -1,28 +1,62 @@
 import express from "express";
 import sharp from "sharp";
+import {
+    deleteProcessedFile,
+    deleteRawFile,
+    downloadRawImage,
+    processImage,
+    setupDirectories,
+    uploadProcessedImage,
+} from "./storage";
+
+setupDirectories();
 
 const app = express();
 app.use(express.json());
 const PORT = 3000;
 
 app.post("/process-image", async (req, res) => {
-    const imagePath = req.body.imagePath;
-    const compressedPath = req.body.outputPath;
-
-    if (!imagePath || !compressedPath) {
-        res.status(400).send("Error: File not found.");
-    }
+    // bucket and filename from cloud pubsub
+    let data;
 
     try {
-        await sharp(imagePath)
-            .resize(800)
-            .toFormat("jpeg", { mozjpeg: true })
-            .toFile(compressedPath + ".jpeg");
+        const message = Buffer.from(req.body.message.data, "base64").toString(
+            "utf8"
+        );
+        data = JSON.parse(message);
 
-        res.status(200).send("Image processed successfully.");
+        if (!data.name) {
+            throw new Error("Invalid message payload received.");
+        }
     } catch (error) {
-        res.status(500).send("Error: image could not be processed.");
+        console.log(error);
+        return res.status(400).send("Bad Request: missing file name");
     }
+
+    const inputFileName = data.name;
+    const outputFileName = `processed-${inputFileName}`;
+
+    await downloadRawImage(inputFileName);
+
+    try {
+        await processImage(inputFileName, outputFileName);
+    } catch (error) {
+        await Promise.all([
+            deleteRawFile(inputFileName),
+            deleteProcessedFile(outputFileName),
+        ]);
+        console.log(`processing error ${error}`);
+        return res.status(500).send("Error: image could not be processed");
+    }
+
+    await uploadProcessedImage(outputFileName);
+
+    await Promise.all([
+        deleteRawFile(inputFileName),
+        deleteProcessedFile(outputFileName),
+    ]);
+
+    return res.status(200).send("Image processed successfully.");
 });
 
 app.listen(PORT, () => {
