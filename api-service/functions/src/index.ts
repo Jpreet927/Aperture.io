5937fac644cb455536ab3aabc829c8be8dfd431a
Jaipreet Singh <jaipreet.singh1@ontariotechu.net>
2023-09-21 18:54:20 -0400

    :fire: more firebase functions for uploading and fetching images

--- api-service/functions/src/index.ts
+++ api-service/functions/src/index.ts
@@ -1,18 +1,17 @@
 /* eslint-disable */
-import { onRequest } from "firebase-functions/v2/https";
 import * as logger from "firebase-functions/logger";
 import * as functions from "firebase-functions";
 import { initializeApp } from "firebase-admin/app";
 import { Firestore } from "firebase-admin/firestore";
+import { Storage } from "@google-cloud/storage";
+import { onCall } from "firebase-functions/v2/https";
 
 initializeApp();
 
 const firestore = new Firestore();
+const storage = new Storage();
 
-export const helloWorld = onRequest((request, response) => {
-    logger.info("Hello logs!", { structuredData: true });
-    response.send("Hello from Firebase!");
-});
+const rawBucketName = "jpreet927-img-raw-images";
 
 export const createUser = functions.auth.user().onCreate((user) => {
     const userInfo = {
@@ -27,4 +26,40 @@ export const createUser = functions.auth.user().onCreate((user) => {
 
     return;
 });
+
+export const generateUploadURL = onCall(
+    { maxInstances: 1 },
+    async (request) => {
+        if (!request.auth) {
+            throw new functions.https.HttpsError(
+                "failed-precondition",
+                "User not authenticated"
+            );
+        }
+
+        const auth = request.auth;
+        const data = request.data;
+        const bucket = storage.bucket(rawBucketName);
+
+        const fileName = `${auth.uid}-${Date.now()}-${data.fileExtension}`;
+
+        const [url] = await bucket.file(fileName).getSignedUrl({
+            version: "v4",
+            action: "write",
+            expires: Date.now() + 15 * 60 * 1000,
+        });
+
+        return { url, fileName };
+    }
+);
+
+const imageCollectionId = "images";
+export const getImages = onCall({ maxInstances: 1 }, async () => {
+    const snapshot = await firestore
+        .collection(imageCollectionId)
+        .limit(20)
+        .get();
+    snapshot.docs.map((doc) => doc.data());
+});
+
 /* eslint-enable */
