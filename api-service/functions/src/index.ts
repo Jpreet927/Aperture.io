5f4e01e7a56aba4f559cc5d0824a926e04867856
Jaipreet Singh <jaipreet.singh1@ontariotechu.net>
2023-09-22 18:00:49 -0400

    deploying cloud functions from mac againnn

--- api-service/functions/src/index.ts
+++ api-service/functions/src/index.ts
@@ -1,10 +1,10 @@
 /* eslint-disable */
 import * as logger from "firebase-functions/logger";
 import * as functions from "firebase-functions";
-import { initializeApp } from "firebase-admin/app";
-import { Firestore } from "firebase-admin/firestore";
-import { Storage } from "@google-cloud/storage";
-import { onCall } from "firebase-functions/v2/https";
+import {initializeApp} from "firebase-admin/app";
+import {Firestore} from "firebase-admin/firestore";
+import {Storage} from "@google-cloud/storage";
+import {onCall} from "firebase-functions/v2/https";
 
 initializeApp();
 
@@ -33,7 +33,7 @@ export const generateUploadURL = onCall(
         if (!request.auth) {
             throw new functions.https.HttpsError(
                 "failed-precondition",
-                "User not authenticated"
+                "User not authenticated",
             );
         }
 
