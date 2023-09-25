7c828e4c5eadff4f49c55fd6a6cf79669ae6c211
Jaipreet Singh <jaipreet.singh1@ontariotechu.net>
2023-09-25 19:33:10 -0400

    :truck: created more routes

---
+++ web-client/app/[userId]/[imageId]/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const ImagePage = ({ params }: { params: { imageId: string } }) => {
+    return <div>ImagePage</div>;
+};
+
+export default ImagePage;
---
+++ web-client/app/[userId]/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const UserPage = ({ params }: { params: { userId: string } }) => {
+    return <div>UserPage</div>;
+};
+
+export default UserPage;
